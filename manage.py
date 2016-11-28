5812998840e5b525a4054022b06c2d8102681fdf
Metin Demir <metmirr@users.noreply.github.com>
2016-11-28 14:05:41 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 from app import create_app, db
 from app.models import City
-
-from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('default')
 manager = Manager(app)
@@ -15,7 +14,6 @@ def make_shell_context():
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
-
 
 if __name__ == '__main__':
     manager.run()
